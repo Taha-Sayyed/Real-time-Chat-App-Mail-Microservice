4309ab231d7cf5dd7dc102e80b549bdeefca4c4a
Taha Sayyed <codetaha786@gmail.com>
2026-05-20 11:29:30 +0530

    feat(mail): add Jest configuration for testing TypeScript files

---
+++ backend/mail/jest.config.ts
@@ -0,0 +1,21 @@
+import type { JestConfigWithTsJest } from 'ts-jest';
+
+const config: JestConfigWithTsJest = {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.ts'],
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  modulePathIgnorePatterns: ['<rootDir>/dist/'],
+};
+
+export default config;
